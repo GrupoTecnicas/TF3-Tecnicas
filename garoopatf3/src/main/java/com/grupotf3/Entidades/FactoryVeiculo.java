96e01f0dfaa4609d1e487d16acc0f66c2f8caf3a
FredericoMartins0 <frederico.martins@edu.pucrs.br>
2019-11-20 17:03:24 -0300

    Atualização FactoryVeiculo

--- garoopatf3/src/main/java/com/grupotf3/Entidades/FactoryVeiculo.java
+++ garoopatf3/src/main/java/com/grupotf3/Entidades/FactoryVeiculo.java
@@ -1,12 +1,13 @@
 package com.grupotf3.Entidades;
 
 public class FactoryVeiculo{
-    public static Veiculo criaVeiculo(String placa, String marca, String cor, CategoriaVeiculo cat){
+    public static Veiculo criaVeiculo(String placa, String marca, String cor, String cat){
         Veiculo vel = null;
-        switch(cat){
-            case SIMPLES: vel = new VeiculoSimples(placa, marca, cor, cat); break;
-            case NORMAL: vel = new VeiculoNormal(placa, marca, cor, cat); break;
-            case LUXO: vel = new VeiculoLuxo(placa, marca, cor, cat); break;
+        CategoriaVeiculo c = CategoriaVeiculo.valueOf(cat);
+        switch(c){
+            case SIMPLES: vel = new VeiculoSimples(placa, marca, cor, c); break;
+            case NORMAL: vel = new VeiculoNormal(placa, marca, cor, c); break;
+            case LUXO: vel = new VeiculoLuxo(placa, marca, cor, c); break;
         }
         return vel;
     }
